a8298f1a3002f958c9261b4a31e1ce66f83ff079
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-03-20 18:25:21 -0300

    Create layout's TransactionAddPage

--- app/src/main/java/br/com/rodrigo/financarte/ui/pages/transaction/add/TransactionAddPage.kt
+++ app/src/main/java/br/com/rodrigo/financarte/ui/pages/transaction/add/TransactionAddPage.kt
@@ -1,15 +1,110 @@
 package br.com.rodrigo.financarte.ui.pages.transaction.add
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import br.com.rodrigo.financarte.ui.theme.FinancarteTheme
 
 @Composable
 fun TransactionAddPage(navHostController: NavHostController) {
-    Text("Transaction Add Page")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(onClick = { navHostController.popBackStack() }) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+            }
+
+            Text(text = "Nova transação", style = MaterialTheme.typography.bodyLarge)
+        }
+
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.padding(top = 15.dp, start = 15.dp)) {
+                Text(text = "Valor da transação", style = MaterialTheme.typography.bodySmall)
+                Text(text = "R$ 0,00", style = MaterialTheme.typography.displaySmall)
+            }
+        }
+
+        Row(
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+                .background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .background(Color.Red)
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.List,
+                    contentDescription = null,
+                    modifier = Modifier.padding(start = 15.dp)
+                )
+
+                BasicTextField(
+                    value = "Descrição",
+                    onValueChange = {},
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 10.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(apiLevel = 33, showSystemUi = true)
